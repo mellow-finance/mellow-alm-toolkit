507b3c4533e69d89cdfa04d6708d7115bffac6a4
Andrey Borzenkov <armoking32@gmail.com>
2024-01-16 13:31:03 +0400

    fix

--- src/modules/univ3/UniV3AmmGnosisModule.sol
+++ src/modules/univ3/UniV3AmmGnosisModule.sol
@@ -2,16 +2,21 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-// import "@zodiac/core/Module.sol";
+import "@zodiac/core/Module.sol";
 
 import "./UniV3AmmModule.sol";
 
-contract UniV3AmmGnosisModule is UniV3AmmModule {
+contract UniV3AmmGnosisModule is UniV3AmmModule, Module {
+    error InvalidState();
+    error Forbidden();
+
     using SafeERC20 for IERC20;
 
     constructor(
         INonfungiblePositionManager positionManager_
     ) UniV3AmmModule(positionManager_) {}
+
+    function setUp(bytes memory) public override {}
 
     function beforeRebalance(
         address,
@@ -24,6 +29,20 @@ contract UniV3AmmGnosisModule is UniV3AmmModule {
     function afterRebalance(
         address,
         address vault,
-        uint256
-    ) external override {}
+        uint256 tokenId
+    ) external override {
+        if (msg.sender != address(this)) revert();
+        (bool success, ) = execAndReturnData(
+            vault,
+            0,
+            abi.encodeWithSelector(
+                positionManager.approve.selector,
+                address(this),
+                tokenId
+            ),
+            Enum.Operation.Call
+        );
+        if (!success) revert InvalidState();
+        positionManager.transferFrom(vault, address(this), tokenId);
+    }
 }
