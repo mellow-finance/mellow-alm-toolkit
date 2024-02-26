f79779f3d24befd0a6e467e1a6680184c4bc9d5e
Andrey Borzenkov <armoking32@gmail.com>
2024-02-26 14:06:02 +0400

    fix

---
+++ src/utils/VeloSugarHelper.sol
@@ -0,0 +1,130 @@
+// SPDX-License-Identifier: BSL-1.1
+pragma solidity ^0.8.0;
+
+import "./VeloDeployFactory.sol";
+
+contract VeloSugarHelper {
+    VeloDeployFactory public factory;
+
+    struct Lp {
+        uint256 amount0;
+        uint256 amount1;
+        uint256 lpAmount;
+        uint256 stakedLpAmount;
+        address almFarm;
+        address almVault;
+        uint256 almFeeD9;
+        address rewardToken;
+        uint256 rewards;
+        uint256 nft;
+        address pool;
+        address token0;
+        address token1;
+        uint256 reserve0;
+        uint256 reserve1;
+        int24 tickSpacing;
+        int24 tick;
+        uint160 price;
+        address gauge;
+        bool initialized;
+    }
+
+    constructor(address _factory) {
+        factory = VeloDeployFactory(_factory);
+    }
+
+    function getData(
+        address user,
+        uint256 id,
+        ICore core
+    ) public view returns (Lp memory lp) {
+        ICore.PositionInfo memory position = core.position(id);
+        IVeloDeployFactory.PoolAddresses memory addresses = factory
+            .poolToAddresses(position.pool);
+        if (
+            addresses.lpWrapper == address(0) ||
+            position.owner != addresses.lpWrapper
+        ) return lp; // empty response
+        if (position.tokenIds.length != 1) return lp; // empty response
+        StakingRewards farm = StakingRewards(addresses.synthetixFarm);
+        lp.lpAmount = IERC20(addresses.lpWrapper).balanceOf(user);
+        lp.stakedLpAmount = farm.balanceOf(user);
+        lp.rewards = farm.earned(user);
+        if (lp.rewards == 0 && lp.stakedLpAmount == 0 && lp.lpAmount == 0)
+            return lp; // empty response
+        lp.almFarm = addresses.synthetixFarm;
+        lp.almVault = addresses.lpWrapper;
+        ICLGauge gauge = ICLGauge(ICLPool(position.pool).gauge());
+        lp.rewardToken = gauge.rewardToken();
+        lp.nft = position.tokenIds[0];
+        lp.token0 = ICLPool(position.pool).token0();
+        lp.token1 = ICLPool(position.pool).token1();
+        lp.tickSpacing = ICLPool(position.pool).tickSpacing();
+        IAmmModule ammModule = core.ammModule();
+        lp.almFeeD9 = IVeloAmmModule(address(ammModule)).protocolFeeD9();
+        lp.pool = position.pool;
+        lp.initialized = true;
+        lp.gauge = ICLPool(position.pool).gauge();
+        {
+            (lp.price, lp.tick, , , , ) = ICLPool(position.pool).slot0();
+            (lp.reserve0, lp.reserve1) = ammModule.tvl(
+                lp.nft,
+                lp.price,
+                lp.pool,
+                lp.almFarm
+            );
+        }
+        uint256 totalSupply = IERC20(addresses.lpWrapper).totalSupply();
+        lp.amount0 = FullMath.mulDiv(
+            lp.reserve0,
+            lp.lpAmount + lp.stakedLpAmount,
+            totalSupply
+        );
+        lp.amount1 = FullMath.mulDiv(
+            lp.reserve1,
+            lp.lpAmount + lp.stakedLpAmount,
+            totalSupply
+        );
+    }
+
+    function full(address user) public view returns (Lp[] memory data) {
+        IVeloDeployFactory.Storage memory s = factory.getStorage();
+        uint256 n = s.immutableParams.core.positionCount();
+        data = new Lp[](n);
+        uint256 iterator = 0;
+        for (uint256 i = 0; i < n; i++) {
+            data[iterator] = getData(user, i, s.immutableParams.core);
+            if (data[iterator].initialized) iterator++;
+        }
+        if (iterator < n) {
+            assembly {
+                mstore(data, iterator)
+            }
+        }
+    }
+
+    function all(
+        address user,
+        uint256 limit,
+        uint256 offset
+    ) public view returns (Lp[] memory data) {
+        Lp[] memory fullData = full(user);
+        if (offset > data.length) return new Lp[](0);
+        if (offset + limit > data.length) {
+            limit = data.length - offset;
+        }
+        data = new Lp[](limit);
+        for (uint256 i = 0; i < limit; i++) {
+            data[i] = fullData[offset + i];
+        }
+    }
+
+    function byIndex(
+        uint256 index,
+        address user
+    ) public view returns (Lp memory lp) {
+        Lp[] memory fullData = full(user);
+        if (index >= fullData.length) return lp;
+        return fullData[index];
+    }
+}
---
+++ test/velo/SugarTest.t.sol
@@ -0,0 +1,219 @@
+// SPDX-License-Identifier: BSL-1.1
+pragma solidity ^0.8.0;
+
+import "./DeployFactoryFixture.sol";
+import "../../src/utils/VeloSugarHelper.sol";
+
+contract Integration is DeployFactoryFixture {
+    using SafeERC20 for IERC20;
+
+    ICLGauge public gauge = ICLGauge(Constants.GAUGE);
+
+    function addRewardToGauge(uint256 amount) public {
+        address voter = address(gauge.voter());
+        address rewardToken = gauge.rewardToken();
+        deal(rewardToken, voter, amount);
+        vm.startPrank(voter);
+        IERC20(rewardToken).safeIncreaseAllowance(address(gauge), amount);
+        ICLGauge(gauge).notifyRewardAmount(amount);
+        vm.stopPrank();
+    }
+
+    function logLpInfo(address user) public {
+        VeloSugarHelper sugar = new VeloSugarHelper(address(deployFactory));
+        VeloSugarHelper.Lp[] memory lps = sugar.full(user);
+
+        // uint256 amount0;
+        // uint256 amount1;
+        // uint256 lpAmount;
+        // uint256 stakedLpAmount;
+        // address almFarm;
+        // address almVault;
+        // uint256 almFeeD9;
+        // address rewardToken;
+        // uint256 rewards;
+        // uint256 nft;
+        // address pool;
+        // address token0;
+        // address token1;
+        // uint256 reserve0;
+        // uint256 reserve1;
+        // int24 tickSpacing;
+        // int24 tick;
+        // uint160 price;
+        // address gauge;
+        // bool initialized;
+
+        for (uint256 i = 0; i < lps.length; i++) {
+            VeloSugarHelper.Lp memory lp = lps[i];
+            string memory logS = string(
+                abi.encodePacked(
+                    "amount0: ",
+                    vm.toString(lp.amount0),
+                    " amount1: ",
+                    vm.toString(lp.amount1),
+                    " lpAmount: ",
+                    vm.toString(lp.lpAmount),
+                    " stakedLpAmount: ",
+                    vm.toString(lp.stakedLpAmount),
+                    " almFarm: ",
+                    vm.toString(lp.almFarm)
+                )
+            );
+            logS = string(
+                abi.encodePacked(
+                    logS,
+                    " almVault: ",
+                    vm.toString(lp.almVault),
+                    " almFeeD9: ",
+                    vm.toString(lp.almFeeD9),
+                    " rewardToken: ",
+                    vm.toString(lp.rewardToken),
+                    " rewards: ",
+                    vm.toString(lp.rewards),
+                    " nft: ",
+                    vm.toString(lp.nft),
+                    " pool: ",
+                    vm.toString(lp.pool),
+                    " token0: ",
+                    vm.toString(lp.token0)
+                )
+            );
+            logS = string(
+                abi.encodePacked(
+                    logS,
+                    " token1: ",
+                    vm.toString(lp.token1),
+                    " reserve0: ",
+                    vm.toString(lp.reserve0),
+                    " reserve1: ",
+                    vm.toString(lp.reserve1),
+                    " tickSpacing: ",
+                    vm.toString(lp.tickSpacing),
+                    " tick: "
+                )
+            );
+            logS = string(
+                abi.encodePacked(
+                    logS,
+                    vm.toString(lp.tick),
+                    " price: ",
+                    vm.toString(lp.price),
+                    " gauge: ",
+                    vm.toString(lp.gauge),
+                    " initialized: ",
+                    vm.toString(lp.initialized)
+                )
+            );
+            console2.log(logS);
+        }
+    }
+
+    function testSynthetixFarm() external {
+        vm.startPrank(Constants.OWNER);
+        deal(Constants.WETH, address(Constants.OWNER), 1 ether);
+        deal(Constants.USDC, address(Constants.OWNER), 2000 * 1e6);
+        IERC20(Constants.WETH).safeApprove(
+            address(deployFactory),
+            type(uint256).max
+        );
+        IERC20(Constants.USDC).safeApprove(
+            address(deployFactory),
+            type(uint256).max
+        );
+
+        vm.expectRevert(abi.encodeWithSignature("PoolNotFound()"));
+        deployFactory.createStrategy(
+            Constants.WETH,
+            Constants.USDC,
+            TICK_SPACING + 1
+        );
+
+        vm.expectRevert(abi.encodeWithSignature("InvalidStrategyParams()"));
+        deployFactory.createStrategy(Constants.WETH, Constants.USDC, 100);
+
+        IVeloDeployFactory.PoolAddresses memory poolAddresses = deployFactory
+            .createStrategy(Constants.WETH, Constants.USDC, TICK_SPACING);
+
+        vm.stopPrank();
+
+        lpWrapper = LpWrapper(poolAddresses.lpWrapper);
+        stakingRewards = StakingRewards(poolAddresses.synthetixFarm);
+
+        vm.startPrank(Constants.DEPOSITOR);
+        deal(Constants.USDC, Constants.DEPOSITOR, 1e6 * 1e6);
+        deal(Constants.WETH, Constants.DEPOSITOR, 500 ether);
+        IERC20(Constants.USDC).safeApprove(
+            address(lpWrapper),
+            type(uint256).max
+        );
+        IERC20(Constants.WETH).safeApprove(
+            address(lpWrapper),
+            type(uint256).max
+        );
+        {
+            (, , uint256 lpAmount) = lpWrapper.deposit(
+                500 ether,
+                1e6,
+                1e3,
+                Constants.DEPOSITOR
+            );
+            logLpInfo(Constants.DEPOSITOR);
+
+            require(lpAmount > 0, "Invalid lp amount");
+            console2.log("Actual lp amount:", lpAmount);
+            lpWrapper.approve(address(stakingRewards), type(uint256).max);
+            stakingRewards.stake(lpWrapper.balanceOf(Constants.DEPOSITOR));
+        }
+        vm.stopPrank();
+
+        addRewardToGauge(10 ether);
+
+        skip(7 days);
+
+        {
+            uint256 treasuryBalanceBefore = IERC20(Constants.VELO).balanceOf(
+                Constants.PROTOCOL_TREASURY
+            );
+
+            uint256 depositorBalanceBefore = IERC20(Constants.VELO).balanceOf(
+                Constants.DEPOSITOR
+            );
+
+            vm.startPrank(Constants.OWNER);
+            lpWrapper.emptyRebalance();
+            stakingRewards.notifyRewardAmount(
+                IERC20(Constants.VELO).balanceOf(address(stakingRewards))
+            );
+
+            skip(1 days);
+
+            vm.stopPrank();
+            vm.startPrank(Constants.DEPOSITOR);
+
+            logLpInfo(Constants.DEPOSITOR);
+            stakingRewards.getReward();
+
+            uint256 depositorBalanceAfter = IERC20(Constants.VELO).balanceOf(
+                Constants.DEPOSITOR
+            );
+
+            uint256 treasuryBalanceAfter = IERC20(Constants.VELO).balanceOf(
+                Constants.PROTOCOL_TREASURY
+            );
+
+            uint256 userRewards = depositorBalanceAfter -
+                depositorBalanceBefore;
+            uint256 protocolRewards = treasuryBalanceAfter -
+                treasuryBalanceBefore;
+
+            uint256 totalRewards = userRewards + protocolRewards;
+            assertTrue(totalRewards > 10 ether - 1 days); // max delta in weis = number of seconds in 1 day
+            assertApproxEqAbs(protocolRewards, 1 ether, 1 wei);
+            assertApproxEqAbs(userRewards, 9 ether, 1 days);
+
+            vm.stopPrank();
+            logLpInfo(Constants.DEPOSITOR);
+        }
+    }
+}
