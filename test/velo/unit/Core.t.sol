5cc74f441a350ab4f4015cd5d0a46ee702bdb2b2
Andrey Borzenkov <armoking32@gmail.com>
2024-03-01 17:47:41 +0400

    fix

--- test/velo/unit/Core.t.sol
+++ test/velo/unit/Core.t.sol
@@ -3,10 +3,17 @@ pragma solidity ^0.8.0;
 
 import "./Fixture.sol";
 
-contract Unit is Test {
+contract Unit is Fixture {
     using SafeERC20 for IERC20;
 
     Core public core;
+    VeloOracle public oracle = new VeloOracle();
+    VeloAmmModule public ammModule =
+        new VeloAmmModule(
+            positionManager,
+            Constants.PROTOCOL_TREASURY,
+            Constants.PROTOCOL_FEE_D9
+        );
 
     function testConstants() external {}
 }
