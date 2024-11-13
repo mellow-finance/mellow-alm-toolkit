23e789c0eda71a05c89daf7cfc5c8fac64f1f35b
Anton Iants <yants.anton@gmail.com>
2024-11-13 14:42:13 +0400

    move contract to src

--- scripts/deploy/DeployOperatorContract.s.sol
+++ scripts/deploy/DeployOperatorContract.s.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.25;
 
-import "./OperatorContract.sol";
+import "../../src/deprecated/OperatorContract.sol";
 import "forge-std/Script.sol";
 
 contract Deploy is Script {
     function run() external {
-        vm.startBroadcast(uint256(bytes32(vm.envBytes("OPTIMISM_PK"))));
+        vm.startBroadcast(uint256(bytes32(vm.envBytes("OPERATOR_PRIVATE_KEY"))));
         OperatorContract operatorContract = new OperatorContract();
         console2.log("operatorContract:", address(operatorContract));
         vm.stopBroadcast();
