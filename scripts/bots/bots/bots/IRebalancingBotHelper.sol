1a21aede30f162bd63ac9979074dda3f37a462ee
Eugene Aristow <eugene.aristow@gmail.com>
2024-11-14 13:19:08 +0300

    Introduce RebalancingBotHelper
    - Take helper logic (view methods) from deprecated PulseVeloBotLazy to
    RebalancingBotHelper
    - Now all the views are hosted in RebalancingBotHelper and swap/call in
    RebalancingBot

---
+++ scripts/bots/bots/bots/IRebalancingBotHelper.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.25;
+
+import "../external/velo/ICLPool.sol";
+import "../external/velo/INonfungiblePositionManager.sol";
+
+interface IRebalancingBotHelper {
+    struct SwapQuoteParams {
+        address tokenIn;
+        address tokenOut;
+        uint256 amountIn;
+    }
+
+    /// @dev returns quotes for swap
+    /// @param pool address of Pool
+    /// @param priceTargetX96 actual price of exchange token0<->token1: 2^96 * amountOut/amountIn
+    /// @return swapQuoteParams contains ecessery amountIn amd amountOut to swap for desired target position
+    function necessarySwapAmountForMint(address pool, uint256 priceTargetX96)
+        external
+        view
+        returns (SwapQuoteParams memory swapQuoteParams);
+
+    /// @dev return current positionId for @param pool
+    function poolManagedPositionInfo(address pool)
+        public
+        view
+        returns (uint256 positionId, ICore.ManagedPositionInfo memory managedPositionInfo);
+
+    /// @dev returns flags, true if rebalance is necessary for @param pool
+    function needRebalancePosition(address pool) public view returns (bool isRebalanceRequired);
+
+}
