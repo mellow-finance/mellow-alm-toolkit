454d2177be4195ea0b4831ac668e608702014f68
armoking32 <armoking32@gmail.com>
2024-11-16 17:07:32 +0900

    gas opts

--- src/libraries/PriceLib320.sol
+++ src/libraries/PriceLib320.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.25;
 import "@openzeppelin/contracts/utils/math/Math.sol";
 
 library PriceLib320 {
+    uint256 private constant Q95 = 1 << 95;
     uint256 private constant Q192 = 1 << 192;
 
     function convertBySqrtPriceX96(uint256 amount0, uint160 sqrtPriceX96)
@@ -17,8 +18,9 @@ library PriceLib320 {
             if (sqrtPriceX96 < type(uint128).max) {
                 return Math.mulDiv(amount0, uint256(sqrtPriceX96) * sqrtPriceX96, Q192);
             }
-            if (amount0 < type(uint128).max && (amount0 * sqrtPriceX96) / sqrtPriceX96 == amount0) {
-                return Math.mulDiv(amount0 * sqrtPriceX96, sqrtPriceX96, Q192);
+            uint256 term = amount0 * sqrtPriceX96;
+            if (term / sqrtPriceX96 == amount0) {
+                return Math.mulDiv(term, sqrtPriceX96, Q192);
             }
         }
 
@@ -29,21 +31,21 @@ library PriceLib320 {
 
         // NOTE: Possible overflow for the case where amount1 is large than type(uint256).max
         amount1 = amount0 * uint256(top64Bits) * top64Bits;
-        amount1 += Math.mulDiv(amount0, topBottom, 1 << 95);
+        amount1 += Math.mulDiv(amount0, topBottom, Q95);
         amount1 += Math.mulDiv(amount0, bottomSqr, Q192);
 
         // Compute the remainder, which represents additional precision from the least significant terms.
         uint256 remainder;
         unchecked {
             // Calculate remainder based on cross terms involving the top and bottom parts of amount0 and sqrtPriceX96
-            remainder = uint256(uint96(amount0 * topBottom << 1)) << 96;
+            remainder = uint192(amount0 * topBottom << 97);
+            remainder += uint192(amount0 & 0xffffffffffffffffffffffff000000000000000000000000)
+                * uint96(bottomSqr) + uint96(amount0) * bottomSqr;
 
-            uint192 amount0Bottom96Bits = uint96(amount0);
-            uint192 amount0Mid96Bits = uint192(amount0) - amount0Bottom96Bits;
-
-            remainder += amount0Mid96Bits * uint96(bottomSqr) + amount0Bottom96Bits * bottomSqr;
+            if (remainder < Q192) {
+                return amount1;
+            }
         }
-
-        amount1 += remainder >> 192;
+        return amount1 + 1;
     }
 }
