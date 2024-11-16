2bc67fb1b6c62482b1157f709622f22a156fb80c
armoking32 <armoking32@gmail.com>
2024-11-16 20:33:31 +0900

    giga opt

--- src/libraries/PriceLib320.sol
+++ src/libraries/PriceLib320.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.25;
 import "@openzeppelin/contracts/utils/math/Math.sol";
 
 library PriceLib320 {
-    uint256 private constant Q95 = 1 << 95;
     uint256 private constant Q192 = 1 << 192;
     uint256 private constant Q190 = 1 << 190;
 
@@ -23,26 +22,25 @@ library PriceLib320 {
             }
         }
 
-        uint64 top64Bits = uint64(sqrtPriceX96 >> 96);
-        uint96 bottom96Bits = uint96(sqrtPriceX96);
-        uint192 bottomSqr = uint192(bottom96Bits) * bottom96Bits;
-        uint160 topBottom = uint160(top64Bits) * bottom96Bits;
+        {
+            uint256 top64Bits;
+            uint256 bottom96Bits;
+            uint256 term;
+            unchecked {
+                top64Bits = uint64(sqrtPriceX96 >> 96);
+                bottom96Bits = uint96(sqrtPriceX96);
+                term = (top64Bits * bottom96Bits << 95) + (bottom96Bits * bottom96Bits >> 2);
+            }
 
-        // NOTE: Possible overflow for the case where amount1 is large than type(uint256).max
-        uint256 term1 = (uint256(topBottom) << 95) + (bottomSqr >> 2);
-        uint8 term2 = uint8(bottomSqr & 3);
-        amount1 = amount0 * top64Bits * top64Bits + Math.mulDiv(amount0, term1, Q190);
-
-        if (term2 & 1 == 1) {
-            amount1 += amount0 >> 192;
-        }
-        if (term2 & 2 == 2) {
-            amount1 += amount0 >> 191;
-        }
-
-        unchecked {
-            if (type(uint192).max ^ (uint192(amount0 * term1) << 2) >= uint192(amount0) * term2) {
-                return amount1;
+            // NOTE: Possible overflow for the case where amount1 is large than type(uint256).max
+            amount1 = amount0 * top64Bits * top64Bits + Math.mulDiv(amount0, term, Q190);
+            unchecked {
+                if (
+                    bottom96Bits & 1 == 0
+                        || type(uint192).max ^ (uint192(amount0 * term) << 2) >= uint192(amount0)
+                ) {
+                    return amount1;
+                }
             }
         }
         return amount1 + 1;
